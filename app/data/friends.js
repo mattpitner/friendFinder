9d7f4efec8d8cefa8c871b327c4f09d12caf05b7
matt <mattpitner09@gmail.com>
2019-04-10 20:57:56 -0600

    Added some routes

---
+++ app/data/friends.js
@@ -0,0 +1,89 @@
+var friends = [
+
+{
+    "name":"Emily",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+
+  {
+    "name":"Adam",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        3,
+        4,
+        1,
+        2,
+        3,
+        5,
+        3,
+        3,
+        4,
+        1
+      ]
+  },
+
+  {
+    "name":"Jordan",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        1,
+        4,
+        3,
+        2,
+        1,
+        4,
+        1,
+        5,
+        2,
+        2
+      ]
+  },
+
+  {
+    "name":"Lucy",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        2,
+        5,
+        2,
+        3,
+        2,
+        1,
+        5,
+        5,
+        2,
+        1
+      ]
+  }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
---
+++ server.js
@@ -0,0 +1,26 @@
+
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory to be served
+app.use(express.static("app/public"));
+
+// Routes
+// =============================================================
+require("./app/routing/apiRoutes")(app);
+
+// Here we introduce HTML routing to serve different HTML files
+require("./app/routing/htmlRoutes")(app);
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});

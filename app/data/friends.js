829e7655076da866848a9f34e061040df2ad0d87
matt <mattpitner09@gmail.com>
2019-04-28 12:05:36 -0600

    made friend image changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,11 +2,11 @@
 var friends = [
   {
     name: "Mart",
-    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjl6Lq3q_PhAhVrxVQKHR80DiwQjRx6BAgBEAU&url=http%3A%2F%2Fbundlestorm.com%2Fdt_gallery%2F2988-people-stock-photos%2F&psig=AOvVaw0Zp6E9NyTPvd1muLG5TNKy&ust=1556559840910532",
+    photo: "./images/mart",
     scores: [
       "5",
       "1",
-      "4",
+      "4", 
       "4",
       "5",
       "1",
@@ -18,7 +18,7 @@ var friends = [
   },
   {
     name: "Kyle",
-    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiF59jPq_PhAhUJllQKHcuzAtAQjRx6BAgBEAU&url=https%3A%2F%2Fwww.freegreatpicture.com%2Fbusiness-class-people%2Fbusiness-people-stock-28321&psig=AOvVaw0Zp6E9NyTPvd1muLG5TNKy&ust=1556559840910532",
+    photo: "./images/kyle",
     scores: [
       "4",
       "2",
@@ -34,7 +34,7 @@ var friends = [
   },
   {
     name: "Lana",
-    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjatbTfq_PhAhVnslQKHTvuD38QjRx6BAgBEAU&url=https%3A%2F%2Fshop.wwe.com%2Flana%2F&psig=AOvVaw0x5CxBXyTbxQcDLzBi3TGJ&ust=1556559928986205",
+    photo: "./images/lana",
     scores: [
       "5",
       "2",
@@ -50,7 +50,7 @@ var friends = [
   },
   {
     name: "Kim",
-    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwipuOX-q_PhAhWR-lQKHWy1Bb8QjRx6BAgBEAU&url=https%3A%2F%2Ftwitter.com%2Fkimstocknyc&psig=AOvVaw0M8Ho3GqWi6wRkLP6BdiiH&ust=1556559994510271",
+    photo: "./images/kim",
     scores: [
       "3",
       "3",
@@ -66,7 +66,7 @@ var friends = [
   },
   {
     name: "Kate",
-    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjUweSSrPPhAhWqxVQKHU--CfcQjRx6BAgBEAU&url=https%3A%2F%2Ftwitter.com%2Fkatecstock&psig=AOvVaw1yPuLyiDZ-QWJc-o_c96nI&ust=1556560029747006",
+    photo: "./images/kate",
     scores: [
       "4",
       "3",
@@ -82,7 +82,7 @@ var friends = [
   },
   {
     name: "Billy",
-    photo: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj43pilrPPhAhVSwZ8KHc8oAK0QjRx6BAgBEAU&url=https%3A%2F%2Ftwitter.com%2Fbilly_stock&psig=AOvVaw2NOntW8Xi18cIyORjEda4U&ust=1556560073911056",
+    photo: "./images/billy",
     scores: [
       "4",
       "4",

3f28f8190db9745d1ddb28f6d92b34c0c9ffe968
matt <mattpitner09@gmail.com>
2019-04-29 14:47:47 -0600

    photo changes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
 
 var friends = [
   {
-    name: "Mart",
-    photo: "../images/mart.jpg",
+    name: "Matt",
+    photo: "https://images.pexels.com/photos/555790/pexels-photo-555790.png?auto=compress&cs=tinysrgb&dpr=1&w=500",
     scores: [
-      "5",
       "1",
-      "4", 
-      "4",
-      "5",
       "1",
-      "2",
-      "5",
-      "4",
+      "1", 
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
       "1"
     ]
   },
